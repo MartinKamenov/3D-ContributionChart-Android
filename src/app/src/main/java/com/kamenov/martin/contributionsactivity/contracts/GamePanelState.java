0033153667b518f2c2241fb1b589b815ed137903
MartinKamenov <martinkamenov10@gmail.com>
2019-02-26 19:38:44 +0200

    Init project. Added 3D engine to app

---
+++ src/app/src/main/java/com/kamenov/martin/contributionsactivity/contracts/GamePanelState.java
@@ -0,0 +1,10 @@
+package com.kamenov.martin.contributionsactivity.contracts;
+
+/**
+ * Created by Martin on 12.11.2018 г..
+ */
+
+public enum GamePanelState {
+    Rotating,
+    Moving
+}
---
+++ src/app/src/main/java/com/kamenov/martin/contributionsactivity/engine/models/game_objects/contracts/GameObject.java
@@ -0,0 +1,11 @@
+package com.kamenov.martin.contributionsactivity.engine.models.game_objects.contracts;
+
+import android.graphics.Canvas;
+
+/**
+ * Created by Martin on 6.3.2018 г..
+ */
+
+public interface GameObject {
+    void update();
+}
