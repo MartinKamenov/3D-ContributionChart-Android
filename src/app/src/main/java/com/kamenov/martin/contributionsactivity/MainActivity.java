01d9c8c047b90c24cdfce9a8fe9c5f48c5f09408
MartinKamenov <martinkamenov10@gmail.com>
2019-02-26 19:50:57 +0200

    Connected gamepannel to app

--- src/app/src/main/java/com/kamenov/martin/contributionsactivity/MainActivity.java
+++ src/app/src/main/java/com/kamenov/martin/contributionsactivity/MainActivity.java
@@ -1,13 +1,35 @@
 package com.kamenov.martin.contributionsactivity;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.RelativeLayout;
 
-public class MainActivity extends AppCompatActivity {
+import com.kamenov.martin.contributionsactivity.engine.GamePanel;
+import com.kamenov.martin.contributionsactivity.engine.services.DrawingService;
+import com.kamenov.martin.contributionsactivity.engine.services.SortingService;
+import com.kamenov.martin.contributionsactivity.engine.services.factories.FigureFactory;
+
+public class MainActivity extends Activity {
+
+    private DrawingService drawingService;
+    private GamePanel gamePanel;
+    private RelativeLayout relativeLayout;
+    private FigureFactory figureFactory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        figureFactory = FigureFactory.getInstance();
+        relativeLayout = findViewById(R.id.container);
+        drawingService = DrawingService.getInstance(SortingService.getInstance());
+        gamePanel = new GamePanel(this, drawingService);
+        relativeLayout.addView(gamePanel);
     }
 }
